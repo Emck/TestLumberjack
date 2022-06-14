77dd99b351398e3552fd7aa91ed89724e5bd4672
Emck <emck.do@gmail.com>
2022-06-14 09:45:25 +0800

    import init example

---
+++ TestLumberjack/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestLumberjack
+//
+//  Created by Emck on 6/14/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestLumberjack/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TestLumberjack
+//
+//  Created by Emck on 6/14/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
